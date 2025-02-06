f55f47fdb3fcc71183cfd22d9ba2e012c170c190
pranjal-walunj <pranjalwalunj132@gmail.com>
2025-02-06 15:59:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -157,7 +157,7 @@ resource "aws_db_instance" "db" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = "admin"
-  password             = "Pass1234"
+  password             = " "
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.mysubnetgp.name
